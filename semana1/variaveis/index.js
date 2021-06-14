3978b2ced3154c4dfbda2422445080a6dc7d9981
Eunice <eunicej429@gmail.com>
2021-06-14 17:12:06 -0300

    semana1-aula03

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,43 @@
+// //let  a  =   10   
+// let  b  =   10  
+// console.log(b)  
+// b  =   5  
+// console.log(a,  b)
+
+// console.log(b) = 10
+
+// console.log(a, b) = 10 5
+
+// 02
+
+// let a = 10
+// let b = 20
+// let c
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// console.log(a, b, c) = 10 10 10
+
+// 03
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// let p = horasDia
+// let t = valorDia
+// alert(`Voce recebe ${valorDia/horasDia} por hora`)
+
+
+
+// exercicios de escrita de codigos
+
+// 1
+let nome
+let idade
+console.log(typeof nome, typeof idade)
+// com o const eu declarei a variavel, porem deu erro porque nao atribui o valor
+// com o let eu declarei a variavel, porem deu certo porque nao precisa atribuir um valor
