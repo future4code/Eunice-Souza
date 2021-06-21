ada9362ffaeb9bf4a6b92323f5a974567c1b55f9
Eunice <eunicej429@gmail.com>
2021-06-21 17:25:20 -0300

    exercicio-objetos

---
+++ semana2/objetos/index.js
@@ -0,0 +1,35 @@
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+//  EXERCICIO 1
+ 
+//   eu acho que na linha 1 vai aparecer filme.elenco[0] vai imprimir Mateus Nachtergaele / eu acho que na linha 2 vai aparecer filme.elenco.length - 1 / eu acho que na linha 3 vai aparecer canal: globo, horario 1
+
+// execicio 2
+const pessoa = {
+    nome: "Eunice",
+    apelidos: ['Nicinha','Nice','Ni']
+ }
+
+ function imprimeApelido(pessoa) {
+     console.log("eu me  chamo" + pessoa.nome + "mas podem me chamar de" + pessoa.apelidos)
+     const apelidos = todosOsApelidos
+
+ }
+ 
+ 
