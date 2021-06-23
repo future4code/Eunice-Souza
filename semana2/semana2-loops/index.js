250d39fb2024ae7e013f98a78b699208feb17d3a
Eunice <eunicej429@gmail.com>
2021-06-23 17:48:36 -0300

    exercicio semana2-loops

---
+++ semana2/semana2-loops/index.js
@@ -0,0 +1,29 @@
+            // interprtando o codigo 01
+   
+//  o valor da saida sera 10
+
+
+            // interpretando codigo 02
+
+    // vai ser impresso 19, 21, 23, 25, 27, 30
+
+            // exercicio 03
+   
+    //  acho que vai ser imprimido +1 ++ 
+    
+    
+            //  exercicio 01
+
+    const quantidadeTotalDePets = Number(prompt('quantos pets voce tem?'))
+    const quantidadeAtualDesPets = 3
+    while(quantidadeTotalDePets => quantidadeAtualDesPets){
+       numero =+ '3' 
+    }
+   console.log(pets)
+   quantidadeAtualDesPets +=3
+   
+             //   exercicio 02  
+              
+let arrayOriginal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(arrayOriginal)
+console.log(arrayOriginal / 10)
