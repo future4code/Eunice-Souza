5ff9886fb7b9b78d6505186285b96ae054095c8c
Eunice <eunicej429@gmail.com>
2021-09-02 09:35:25 -0300

    projeto-labedite

--- projeto-labeddit/src/pages/LoginPage/LoginPage.js
+++ projeto-labeddit/src/pages/LoginPage/LoginPage.js
@@ -1,79 +1,82 @@
-import React from "react"
-import axios from "axios"
+import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-const LoginPage =() => {
-    const [email, setEmail] = React.useState('')
-    const [senha, setSenha] = React.useState('')
-    const pegaTextosEmail = (event) => {
-        setEmail(event.target.value)
+const LoginPage = () => {
+  const [email, setEmail] = React.useState("");
+  const [senha, setSenha] = React.useState("");
+
+  const history = useHistory();
+
+  const pegaTextosEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const pegaTextosSenha = (event) => {
+    setSenha(event.target.value);
+  };
+  const LoginUsuario = async (event) => {
+    event.preventDefault();
+
+    const url = "https://labeddit.herokuapp.com/users/login";
+    const body = {
+      email: email,
+      password: senha,
+    };
+    const headers = {
+      headers: {
+        ContentType: "application/json",
+      },
+    };
+
+    let response;
+    try {
+      response = await axios.post(url, body, headers);
+    } catch (err) {
+      response = null;
+      alert("error");
+    } finally {
+      if (response !== null) {
+        history.push("/");
+      }
+      console.log(response.data.token)
+      localStorage.setItem("token", response.data.token)
+      return response;
     }
-    const pegaTextosSenha = (event) => {
-        setSenha(event.target.value)
-    }
-    const loginUsuario = async (event) => {
-        event.preventDefault()
+  };
 
-        const url = 'https://labeddit.herokuapp.com';
-        const body = {
-            email: email,
-	        password: senha
-        }
-        const headers = {
-            headers: {
-                ContentType: "application/json"
-            }
-        }
+  return (
+    <div>
+      <h1>LoginPage</h1>
 
-        let response;
-        try {
-            response = await axios.post(url, body, headers);
-        } catch (err){
-            response = null;
-            alert("error");
-        } finally {
-            console.log(response)
-            return response
-        }
+      <form onSubmit={LoginUsuario}>
+        <input
+          name="email"
+          required
+          onChange={pegaTextosEmail}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          name="senha"
+          required
+          onChange={pegaTextosSenha}
+          type="password"
+          placeholder="senha"
+        />
+        <button type="submit">Entrar</button>
+        <button>Cadastrar</button>
+      </form>
+    </div>
+  );
+};
 
-        
-    }
-
-    return (
-        <div>
-            
-            <h1>LoginPage</h1>
-
-            <form onSubmit={loginUsuario}>
-                <input
-                    name="email"
-                    required
-                    onChange={pegaTextosEmail}
-                    type="email"
-                    placeholder="Email"
-                />
-                <input
-                    name="senha"
-                    required
-                    onChange={pegaTextosSenha}
-                    type="password"
-                    placeholder="senha"
-                />
-                <button type="submit">Entrar</button>
-                <button>Cadastrar</button>
-            </form>
-
-                  
-        </div>
-    )
-}
-
-export default LoginPage
+export default LoginPage;
 
 // 1 - Obter texto escrito pelo usuario atravez dos inputs.
 //  passo um - criar uma função responsavel por obter o texto escrito pelo usuario.
 //  passo dois - executar a função criada toda vez que o usuario digitar no input, atravez
 // do evento onChange.
-//  passo tres - dentro da função criada, obter os textos dos inputs atravez do parametro 
+//  passo tres - dentro da função criada, obter os textos dos inputs atravez do parametro
 // event.
 // passo quatro - criar um estado para cada input e armazenar os textos digitados nos estados.
 
--- projeto-labeddit/src/pages/listPostPage/ListPostPage.js
+++ projeto-labeddit/src/pages/listPostPage/ListPostPage.js
@@ -1,12 +1,71 @@
+import axios from "axios";
 import React from "react";
 
+const ListPostPage = () => {
+  const [post, setPost] = React.useState("");
+  const [titulo, setTitulo] = React.useState("");
 
-const ListPostPage =() => {
-    return (
-        <div>
-            <h1>ListPostPage</h1>      
-        </div>
-    )
-}
+  const pegaTextoPost = (event) => {
+      const valorDigitado = event.target.value
+    setPost(valorDigitado);
+   };
 
-export default ListPostPage
+  const pegaTituloPost = (event) => {
+    setTitulo(event.target.value);
+   
+  };
+
+  const PostUsuario = async (event) => {
+    event.preventDefault();
+
+    const url = "https://labeddit.herokuapp.com/posts";
+
+    const body = {
+      title: titulo,
+      body: post,
+    };
+    const token = localStorage.getItem('token')
+
+    const headers = {
+      headers: {
+        ContentType: "application/json",
+        Authorization: token,
+      },
+    };
+
+    let response;
+    try {
+      response = await axios.post(url, body, headers);
+    } catch (err) {
+      response = null;
+      alert("error");
+    } finally {
+        return response;
+    }
+  };
+
+  return (
+    <div>
+      <h1>ListPostPage</h1>
+      <form onSubmit={PostUsuario}>
+        <input
+          name="Post"
+          required
+          onChange={pegaTextoPost}
+          type="text"
+          placeholder="Escreva seu post"
+        />
+        <input
+          name="title"
+          required
+          onChange={pegaTituloPost}
+          type="text"
+          placeholder="title do post"
+        />
+        <button type="Submit">Postar</button>
+      </form>
+    </div>
+  );
+};
+
+export default ListPostPage;
