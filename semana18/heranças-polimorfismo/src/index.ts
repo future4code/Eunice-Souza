b938b2538c10b1d06e4eca617ef3933dc04d3247
Eunice <eunicej429@gmail.com>
2021-10-13 17:10:02 -0300

    execicio sobre Heranças e Polimorfismo

---
+++ semana18/heranças-polimorfismo/src/index.ts
@@ -0,0 +1,22 @@
+// import app from "./app"
+// import createCharacter from "./endpoints/createCharacter"
+// import deleteCharacter from "./endpoints/deleteCharacter"
+// import getAllCharacters from "./endpoints/getAllCharacters"
+
+
+
+// app.get("/character", getAllCharacters)
+// app.post("/character", createCharacter)
+// app.delete("/character/:id", deleteCharacter)
+// --------------------------------------------------
+
+// # execício 1 #
+
+// a. *Seria possível imprimir a senha (`password`) atrelada a essa instância?*
+// *Resposta: Não sera possivel imprimir a senha, entrelada a esta instancia por está é privada.
+
+// b. *Quantas vezes a mensagem `"Chamando o construtor da classe User"` foi impressa no terminal?*
+// *Resposta: Esta mensagem foi impressa no consoleuma vez.
+ 
+
+console.log("User")
